3936d5d65d1cee076496f0510bc8e906dc724b38
israel <israel_brian@hotmail.com>
2025-11-24 15:12:41 -0300

    Correção na responsividade da page interactiveCards
    - Implementação de melhor responsividade.
    - Tratamento de tamanho de fontes, elementos e fixação da imagem usada de bg retirando a barra lateral "scroll"

--- app/src/pages/InteractiveCards.jsx
+++ app/src/pages/InteractiveCards.jsx
@@ -21,24 +21,24 @@ const InteractiveCards = () => {
   return (
     <div
       style={{ backgroundImage }}
-      className="min-h-screen bg-cover bg-center bg-no-repeat flex flex-col justify-center items-center animate-fade-in relative p-4"
+      className="absolute top-0 left-0 w-full h-full overflow-hidden min-h-screen bg-cover bg-center bg-no-repeat flex flex-col justify-center items-center animate-fade-in p-4"
     >
       <Link
         to="/home"
-        className="absolute top-4 left-4 md:top-10 md:left-12 text-white p-5 rounded-full bg-brand-green hover:bg-brand-green-light transition-colors duration-300 shadow-lg"
+        className="absolute top-4 left-4 md:top-12 md:left-12 text-white p-5 rounded-full bg-brand-green hover:bg-brand-green-light transition-colors duration-300 shadow-lg"
         aria-label="Voltar para a página inicial"
       >
         <FaArrowLeft size={20} />
       </Link>
 
       <header className="text-center mb-12">
-        <h1 className="text-4xl md:text-6xl font-black text-gray-200 mb-4 -tracking-tight [text-shadow:4px_3px_3px_#084808]">
+        <h1 className="text-4xl md:text-6xl font-black text-gray-200 mt-2 md:mt-0 mb-4 -tracking-tight [text-shadow:4px_3px_3px_#084808]">
           Cards Informativos
         </h1>
       </header>
 
       <div className="bg-brand-green-dark p-4 rounded-lg shadow-lg flex flex-col items-center w-auto">
-        <div className="relative w-[300px] h-[450px] md:w-[350px] md:h-[525px] lg:w-[400px] lg:h-[600px]">
+        <div className="relative w-[300px] h-[450px] md:w-[300px] md:h-[400px] 2xl:w-[400px] 2xl:h-[600px]">
           <img
             src={cardImages[currentPage]}
             alt={`Card interativo ${currentPage + 1}`}
